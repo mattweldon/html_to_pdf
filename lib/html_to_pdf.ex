8f789853f264dd90710dbb0ca6a2e0d68f0ac26c
Matt Weldon <matt.weldon@gmail.com>
2015-10-27 16:51:39 +0000

    add more options functions / tests

--- lib/html_to_pdf.ex
+++ lib/html_to_pdf.ex
@@ -9,4 +9,34 @@ defmodule HtmlToPdf do
     |> HtmlToPdf.Document.put_option("--dpi", dpi |> Integer.to_string)
   end
 
+  def set_top_margin(document, top_margin) do
+    document
+    |> HtmlToPdf.Document.put_option("--margin-top", top_margin |> Integer.to_string)
+  end
+
+  def set_right_margin(document, right_margin) do
+    document
+    |> HtmlToPdf.Document.put_option("--margin-right", right_margin |> Integer.to_string)
+  end
+
+  def set_bottom_margin(document, bottom_margin) do
+    document
+    |> HtmlToPdf.Document.put_option("--margin-bottom", bottom_margin |> Integer.to_string)
+  end
+
+  def set_left_margin(document, left_margin) do
+    document
+    |> HtmlToPdf.Document.put_option("--margin-left", left_margin |> Integer.to_string)
+  end
+
+  def print_portrait(document) do
+    document
+    |> HtmlToPdf.Document.put_option("--orientation", "Portrait")
+  end
+
+  def print_landscape(document) do
+    document
+    |> HtmlToPdf.Document.put_option("--orientation", "Landscape")
+  end
+
 end
--- test/html_to_pdf_test.exs
+++ test/html_to_pdf_test.exs
@@ -22,4 +22,76 @@ defmodule HtmlToPdfTest do
 
     assert document.options == %{"--dpi" => "700"}
   end
+
+  test "adds the top margin of the document to the options map" do
+    document = %HtmlToPdf.Document{}
+    |> HtmlToPdf.set_top_margin(42)
+
+    assert document.options == %{"--margin-top" => "42"}
+
+    document = %HtmlToPdf.Document{}
+    |> HtmlToPdf.set_top_margin(43)
+
+    assert document.options == %{"--margin-top" => "43"}
+  end
+
+  test "adds the right margin of the document to the options map" do
+    document = %HtmlToPdf.Document{}
+    |> HtmlToPdf.set_right_margin(42)
+
+    assert document.options == %{"--margin-right" => "42"}
+
+    document = %HtmlToPdf.Document{}
+    |> HtmlToPdf.set_right_margin(43)
+
+    assert document.options == %{"--margin-right" => "43"}
+  end
+
+  test "adds the bottom margin of the document to the options map" do
+    document = %HtmlToPdf.Document{}
+    |> HtmlToPdf.set_bottom_margin(42)
+
+    assert document.options == %{"--margin-bottom" => "42"}
+
+    document = %HtmlToPdf.Document{}
+    |> HtmlToPdf.set_bottom_margin(43)
+
+    assert document.options == %{"--margin-bottom" => "43"}
+  end
+
+  test "adds the left margin of the document to the options map" do
+    document = %HtmlToPdf.Document{}
+    |> HtmlToPdf.set_left_margin(42)
+
+    assert document.options == %{"--margin-left" => "42"}
+
+    document = %HtmlToPdf.Document{}
+    |> HtmlToPdf.set_left_margin(43)
+
+    assert document.options == %{"--margin-left" => "43"}
+  end
+
+  test "sets the orientation of the document portrait in the options map" do
+    document = %HtmlToPdf.Document{}
+    |> HtmlToPdf.print_portrait
+
+    assert document.options == %{"--orientation" => "Portrait"}
+
+    document = %HtmlToPdf.Document{}
+    |> HtmlToPdf.print_portrait
+
+    assert document.options == %{"--orientation" => "Portrait"}
+  end
+
+  test "sets the orientation of the document landscape in the options map" do
+    document = %HtmlToPdf.Document{}
+    |> HtmlToPdf.print_landscape
+
+    assert document.options == %{"--orientation" => "Landscape"}
+
+    document = %HtmlToPdf.Document{}
+    |> HtmlToPdf.print_landscape
+
+    assert document.options == %{"--orientation" => "Landscape"}
+  end
 end
